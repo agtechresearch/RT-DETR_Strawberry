12a8f75225ef35cf0e319ff6f170303052224500
박현지 <74248395+HyeonJi-P@users.noreply.github.com>
2024-03-28 20:14:50 +0900

    Update inference_strawberry.bash

--- inference_strawberry.bash
+++ inference_strawberry.bash
@@ -3,5 +3,5 @@ python3 ./rtdetr_pytorch/tools/inference_and_save_results.py \
     --resume "./rtdetr_pytorch/alarad_str_best_weight_c3/checkpoint0119.pth" \
     --data ../ALARAD_Strawberry_Dataset_final/ALARAD_Strawberry_1060_c1/images/test \
     --save_dir ./rtdetr_pytorch/output
-    --dir_name output \
+    --dir_name predictions \
     --save_img true --save_txt true
